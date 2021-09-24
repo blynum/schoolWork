4f7879680ae6e9b76b5e688d096e8ef70008d79e
Belita Lynum <belita.lynum1@gmail.com>
2021-09-24 17:08:52 -0400

    Adding files

---
+++ node-calculator/app.js
@@ -0,0 +1,46 @@
+
+
+var readlineSync = require('readline-sync');
+var num1 = parseInt(readlineSync.question('May I have your 1st number? '));
+var num2 = parseInt(readlineSync.question('May I have your 2nd number? '));
+var operation = (readlineSync.question('Please enter the operation to perform? '));
+
+
+if(operation == 'add') {
+    console.log(add (num1, num2))
+} else if(operation == 'sub') {
+    console.log(sub(num1, num2))
+} else if (operation == 'mul') {
+    console.log(mul (num1, num2))
+}else if (operation == 'div') {
+    console.log(div (num1, num2))
+} 
+
+function add (num1,num2) {
+   return (num1 + num2)
+}
+
+// var result = add(10,5)
+// console.log(result)
+
+
+function sub (num1,num2) {
+    return num1 - num2
+}
+
+// var result = sub(10,5)
+// console.log (result)
+
+function mul (num1, num2){
+    return num1 * num2
+}
+
+// var result = mul(10,5)
+// console.log (result)
+
+function div (num1, num2) {
+   return num1 / num2
+}
+
+// var result = div (10,5)
+//     console.log (result)
