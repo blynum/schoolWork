682e93e7f54b05bf36c99d3cfa10c72c17741183
Belita Lynum <belita.lynum1@gmail.com>
2021-10-10 11:35:51 -0400

    Writing what I know to start this project

---
+++ colossalAdven/adven.js
@@ -0,0 +1,55 @@
+// Starting Line
+const user = {
+    alive: true,
+
+}
+
+// const readline = require("readline-sync")
+// Wait for user's response
+const readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+const userName = readlineSync.question('May I have your name? ');
+console.log('Welcome ' + userName + ' to the greatest adventure of your life');
+while
+// if
+(readlineSync.keyIn('Please press W to walk...', {limit: "w"})) {
+    // "w" key was pressed
+    console.log('walking'); 
+}
+
+// while(user presses "w" walking will continue to show)
+
+// while(stillWalk === pressing the "w"){
+    // const stillWalk = console.log('still walking')
+// }
+
+// function getRandomNum(max) {
+//     return Math.floor(Math.random() * max);
+// }
+
+// console.log(getRandomNum(3));
+
+// At a random time enemies will appear
+function enemy (){
+    const enemies = [Charizard, Lucario, Jigglypuff]
+    enemies[Math.floor(Math.random() * enemies.length)];
+}
+
+
+
+// user will be given the option to attack or run
+function userOptions(attack, run){
+    // const userAction = [attack, run]
+if(){
+
+}
+if(){
+
+}
+
+}
+
+
+
+
---
+++ proSolPractice/prosol.js
@@ -0,0 +1,18 @@
+// Write a function that takes an array of numbers and returns the largest (without using Math.max())
+let nums = [];
+let largest = 0;
+
+function largestNum(){
+    for (let i = 0; i <= nums.length; i++){
+        if (nums[i]>largest) {
+            let largest=nums[i]
+        }
+    }
+}
+
+// Write a function that takes an array of words and a character and returns each word that has that character present.
+
+
+
+// Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
+
