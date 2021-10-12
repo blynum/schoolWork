2c472b6f0117fe5d5cad9f2168e9bdc8c93f3200
Belita Lynum <belita.lynum1@gmail.com>
2021-10-12 18:02:21 -0400

    Built skeleton of adventure

--- colossalAdven/adven.js
+++ colossalAdven/adven.js
@@ -1,7 +1,15 @@
 // Starting Line
 const user = {
     alive: true,
+    inventory: [ ],
+    hp: 100
 
+}
+
+// At a random time enemies will appear and attack
+function enemyAttack (){
+    const enemies = [Charizard, Lucario, Jigglypuff]
+    return enemies[Math.floor(Math.random() * enemies.length)];
 }
 
 // const readline = require("readline-sync")
@@ -11,12 +19,32 @@ const readlineSync = require('readline-sync');
 // Wait for user's response.
 const userName = readlineSync.question('May I have your name? ');
 console.log('Welcome ' + userName + ' to the greatest adventure of your life');
-while
+while(user.alive){
+    
+    readlineSync.keyIn('Please press W to walk...', {limit: "w"})
+    if(enemyAttack < 2) {
+        function userOptions(attack, run){
+            
+        }
+        
+
+    } else {
+        console.log('you keep walking')
+    }
+    user.alive = false
+}
 // if
-(readlineSync.keyIn('Please press W to walk...', {limit: "w"})) {
     // "w" key was pressed
     console.log('walking'); 
-}
+
+
+// User Presses "w" and get console.log "walking"
+// document.addEventListener('keydown', logKey);
+// function logKey(e) {
+// console.log(e.key);
+// if(e.key=="w")console.log("gotcha!");
+// }
+
 
 // while(user presses "w" walking will continue to show)
 
@@ -30,26 +58,28 @@ while
 
 // console.log(getRandomNum(3));
 
-// At a random time enemies will appear
-function enemy (){
-    const enemies = [Charizard, Lucario, Jigglypuff]
-    enemies[Math.floor(Math.random() * enemies.length)];
-}
+
+
 
 
 
 // user will be given the option to attack or run
-function userOptions(attack, run){
     // const userAction = [attack, run]
-if(){
+// if(){
 
-}
-if(){
+// }
+// if(){
 
-}
+// }
 
-}
-
-
+// }
 
 
+// If player kills the enemy give player some HP and special item. *Should use a while loop
+
+
+// If player dies,
+
+
+
+// If player presses "p" or print,
