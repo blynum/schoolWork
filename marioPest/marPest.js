c3872763468db4a1012f6d7360ef67c196c4ee77
Belita Lynum <belita.lynum1@gmail.com>
2021-10-22 00:11:27 -0400

    Added more CSS and started JS file

--- marioPest/marPest.js
+++ marioPest/marPest.js
@@ -3,16 +3,25 @@ const form = document.marioForm;
 function gooPest(pest1) {
   return pest1 * 5;
 }
-function booPest(pest2) {
+
+function bobPest(pest2) {
   return pest2 * 7;
 }
 function cheePest(pest3) {
   return pest3 * 11;
 }
 
+function sum(gooPest, bobPest, cheePest) {
+  return gooPest + bobPest + cheePest;
+}
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const gooPest = form.gooPest.value;
-  const booPest = form.booPest.value;
+  const bobPest = form.bobPest.value;
   const cheePest = form.cheePest.value;
+  //   const total = sum(gooPest, bobPest, cheePest);
+  form.gooPest.value = "";
+  form.bobPest.value = "";
+  form.cheePest.value = "";
 });
