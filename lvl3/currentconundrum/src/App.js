86d55d9e77a5eac9aaa171bfc9c41e3df0c27461
Belita Lynum <belita.lynum1@gmail.com>
2021-12-09 16:39:00 -0500

    Practicing React State

--- lvl3/currentconundrum/src/App.js
+++ lvl3/currentconundrum/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+// Without React State
+
+// function App() {
+//     const thingsArray = ["Things 1", "Things 2"]
+//     const thingsItems = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+
+//     function addItem() {
+//         const newThingText = `Thing ${thingsArray.length + 1}`
+//         thingsArray.push(newThingText)
+//         console.log(thingsArray)
+//     }
+
+//     return (
+//         <div>
+//             <button onClick={addItem}>Add Item</button>
+//             {thingsItems}
+//         </div>
+//     )
+// }
+
+// With React State
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+
+    function addItem() {
+        const newThingText = `Thing ${things.length + 1}`
+        setThings(prevState => [...prevState, newThingText])
+    }
+
+    const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
+
+    return (
+        <div>
+            <button onClick={addItem}>Add Item</button>
+            {thingsElements}
+        </div>
+    )
 }
 
-export default App;
+export default App
