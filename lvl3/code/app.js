3a67de9001c7472e8020d9c2e79e50dc3f3ebc94
Belita Lynum <belita.lynum1@gmail.com>
2021-12-14 16:47:45 -0500

    Having problems with React

--- lvl3/code/app.js
+++
@@ -1,7 +0,0 @@
-var countdown = 20
-
-while (countdown >= 0) {
-    console.log(countdown)
-    countdown--
-}
-
--- lvl3/digitalBus/digital-bus/src/Components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Navbar() {
-    return (
-        <nav>
-            <img src="airbnb-1.png" alt="AirBnb logo" className="nav--logo" />
-        </nav>
-    )
-}
