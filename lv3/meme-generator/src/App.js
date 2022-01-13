9562f347f9495c198b6d051f682ee4c159897784
Belita Lynum <belita.lynum1@gmail.com>
2022-01-12 20:07:32 -0500

    Learned how to import fonts and text shadowing

--- lv3/meme-generator/src/App.js
+++ lv3/meme-generator/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import './App.css';
 import Header from "./Components/Header"
+import Meme from "./Components/Meme"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
