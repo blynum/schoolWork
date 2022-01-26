243a891380353e338b27c8617566d1a29ee7ad28
Belita Lynum <belita.lynum1@gmail.com>
2022-01-26 14:09:53 -0500

    Practice using React state

--- lv3/react-dice/src/App.js
+++ lv3/react-dice/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import DiceBox from "./DiceBox"
 
 function App() {
+
+  const [numbers, setNumbers] = React.useState([1, 2, 3, 4, 5])
+
+  const arrayOfNumbers = numbers.map((number, index) =>
+    <DiceBox
+      key={index}
+      number={number}
+    />
+  )
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * 6 + 1)
+  }
+  // console.log(generateNumbersArray())
+
+  const rollDice = () => {
+    // passing in new state
+    // setNumbers([
+    //   generateRandomNumber(),
+    //   generateRandomNumber(),
+    //   generateRandomNumber(),
+    //   generateRandomNumber(),
+    //   generateRandomNumber(),
+    // ])
+
+    //1. passing in callback function 
+
+    setNumbers((prevNumbers) => {
+      return prevNumbers.map((number) => generateRandomNumber())
+    })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>React Dice
+        <button onClick={rollDice}>Get Random Numbers</button>
       </header>
+
+      {/* <DiceBox number={numbers[0]} />
+      <DiceBox number={numbers[1]} />
+      <DiceBox number={numbers[2]} />
+      <DiceBox number={numbers[3]} />
+      <DiceBox number={numbers[4]} />
+      <DiceBox number={numbers[5]} /> */}
+      {arrayOfNumbers}
+
     </div>
   );
 }
