9f14c8d0c607e7c2c48086eb489a8b615e61d275
Belita Lynum <belita.lynum1@gmail.com>
2022-02-08 13:58:27 -0500

    Working on Name Badge Assignment

--- lv3/name-entry/src/App.js
+++ lv3/name-entry/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   const [formData, setFormData] = React.useState(
     {
-      comments: ""
+      name: ""
     })
 
   function handleChange(event) {
+    event.preventDefault()
     const { name, value } = event.target
     setFormData(prevFormData => {
       return {
@@ -16,24 +17,32 @@ function App() {
       }
     })
   }
+
+  function handleClick(event) {
+    event.preventDefault()
+    // document.getElementById("myText").value = "";
+    const h1 = document.createElement('h1')
+    h1.textContent = formData
+    document.getElementsByTagName("body")[0].append(h1)
+
+    console.log(handleClick)
+  }
   return (
     <div className="App">
       <h1>Name Entry Assignment</h1>
       <form className="form">
-        <textarea
+        <input
+          id="myText"
           placeholder="Type Name"
           onChange={handleChange}
-          name="comments"
-          value={formData.comments}
+          name="name"
+          value={formData.name}
         />
         {/* <h1>update DOM to show what was put in the input box</h1> */}
-        <button className="form--button">Submit</button>
-        <ol>
-          <li>Jane Doe</li>
-        </ol>
+        <button onClick={handleClick} className="form--button">Submit</button>
       </form>
     </div>
   );
 }
 
-export default App;
+export default App
